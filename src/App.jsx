aceae15bccdc967be40fa382cf68e2a9d356391d
alexbaumgertner <alex.baumgertner@gmail.com>
2018-12-02 13:40:59 +0300

    Add CRA sceleton

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App__link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
