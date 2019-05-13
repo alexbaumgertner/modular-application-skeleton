d69d99ef4b8af5bce3e6974ee26ba36dad3325b9
alexbaumgertner <alex.baumgertner@gmail.com>
2019-05-13 11:16:55 +0400

    Improve mock data

--- utils/dev/generate-mock-data.js
+++ utils/dev/generate-mock-data.js
@@ -5,33 +5,6 @@ const fs = require('fs')
 const path = require('path')
 const faker = require('faker')
 
-const generateUUID = () => faker.fake('{{random.uuid}}')
-
-const generateCatalogItem = () => {
-  return {
-    id: generateUUID(),
-  }
-}
-
-const generateUser = () => {
-  return {
-    id: generateUUID(),
-    name: faker.fake('{{name.lastName}}, {{name.firstName}}'),
-  }
-}
-
-const generateFavorite = () => {
-  return {
-    id: generateUUID(),
-  }
-}
-
-const generateProfile = () => {
-  return {
-    id: generateUUID(),
-  }
-}
-
 const runNTimes = (count = 1) => funcToRun => {
   const result = []
   for (let i = 0; i < count; i++) {
@@ -39,12 +12,49 @@ const runNTimes = (count = 1) => funcToRun => {
   }
   return result
 }
+const getRandomNumber = count => Math.floor(Math.random() * count)
+const generateUUID = () => faker.fake('{{random.uuid}}')
+
+const generateCatalogItem = () => {
+  return {
+    id: generateUUID(),
+    name: faker.fake('{{commerce.productName}}'),
+    price: faker.fake('{{commerce.price}}'),
+    productAdjective: faker.fake('{{commerce.productAdjective}}'),
+    department: faker.fake('{{commerce.department}}'),
+    date: faker.fake('{{date.past}}'),
+  }
+}
+
+const catalog = runNTimes(10)(generateCatalogItem)
+
+const generateUser = () => {
+  return {
+    id: generateUUID(),
+    avatar: faker.fake('{{image.avatar}}'),
+    name: faker.fake('{{name.lastName}}, {{name.firstName}}'),
+    email: faker.fake('{{internet.email}}'),
+    password: faker.fake('{{internet.password}}'),
+    phoneNumber: faker.fake('{{phone.phoneNumber}}'),
+  }
+}
+
+const users = runNTimes(10)(generateUser)
+
+const generateProfile = () => {
+  return {
+    ...(users[getRandomNumber(10)]),
+    favorites: [
+      catalog[getRandomNumber(10)].id,
+      catalog[getRandomNumber(10)].id,
+    ],
+  }
+}
 
 const data = {
-  catalog: runNTimes(10)(generateCatalogItem),
-  users: runNTimes(10)(generateUser),
-  favorites: runNTimes(10)(generateFavorite),
-  profile: runNTimes(3)(generateProfile),
+  catalog,
+  users,
+  profile: runNTimes(1)(generateProfile),
 }
 
 fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(data))
